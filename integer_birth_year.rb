1b1957aee3f4d365b26d65dc5e69f293042c0ec6
priyankac41 <pchandr1@chicagobooth.edu>
2023-01-17 03:00:11 +0000

    birth year

--- integer_birth_year.rb
+++ integer_birth_year.rb
@@ -6,4 +6,8 @@
 # Key output:
 #   "Wow, you were born in 1940. You're old!"
 
+age= gets.chomp.to_i
 p "How old are you?"
+age
+year = 2023-age
+p "Wow, you were born in "+year.to_s+". You're old!"
